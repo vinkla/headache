f23785f5cee0208c8d9dc8b79c47329f9098e2aa
Vincent Klaiber <vincent@doubledip.se>
2023-11-08 19:31:45 +0100

    Update copyright header

--- headache.php
+++ headache.php
@@ -1,10 +1,12 @@
 <?php
 
 /**
- * Copyright (c) Vincent Klaiber.
+ * Copyright (c) Vincent Klaiber
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
+ *
+ * @see https://github.com/vinkla/headache
  */
 
 /*
