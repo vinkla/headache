606bf3808cf7f526fedb3cba3644a6d816db5eb9
Vincent Klaiber <vincent@doubledip.se>
2025-11-18 18:53:53 +0100

    fix: prepare 3.5.4

--- headache.php
+++ headache.php
@@ -14,7 +14,7 @@
  * Description: An easy-to-swallow painkiller plugin for WordPress.
  * Author: Vincent Klaiber
  * Author URI: https://github.com/vinkla
- * Version: 3.5.3
+ * Version: 3.5.4
  * Plugin URI: https://github.com/vinkla/headache
  * GitHub Plugin URI: vinkla/headache
  */
